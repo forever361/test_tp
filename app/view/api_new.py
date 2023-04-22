5ddb7a47070719023c67430f2c572dbf32e3c2cd
forever361 <155178977@qq.comn>
2023-04-22 18:45:18 +0800

    api-v3-frontUI

--- app/view/api_new.py
+++ app/view/api_new.py
@@ -1,18 +1,80 @@
+import json
 
-from flask import Blueprint,render_template, request
+import requests
+from flask import Blueprint, render_template, request, jsonify
 
 import os
 import sys
+
 basePath = os.path.join(os.path.join(os.path.dirname(__file__)))
-configPath = os.path.abspath(os.path.join(os.path.dirname(__file__),"../"))
+configPath = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.append(configPath)
 
-web = Blueprint('apinew', __name__,template_folder='templates/apitest')
+web = Blueprint('apinew', __name__, template_folder='templates/apitest')
 
 
-@web.route('/api_intergration', methods=['GET','POST'])
+@web.route('/api_intergration', methods=['GET', 'POST'])
 # @user.authorize
 def test_api():
     api_name = request.args.get('apiName', '')
-    return render_template('api/api_intergration.html',api_name= api_name)
+    return render_template('api/api_intergration.html', api_name=api_name)
+
+
+@web.route('/get_api_detail', methods=['POST'])
+def get_api_detail():
+    data = request.json
+    if data["apiname"] == 'taobao.appstore.subscribe.get':
+        response_data = [{
+            "apiurl": "http://127.0.0.1:8889/data3.json",
+            "headers": {
+                "content-type": "application/json",
+            },
+            "request_params": {
+                "apiname": "taobao.appstore.subscribe.get"
+            }
+        }]
+    else:
+        response_data = {
+            "apiurl": "http://gw.api.taobao.com/router/rest?app_key=12129701&method=taobao.appstore.subscribe.get&v=2.0",
+            "headers": {
+                "content-type": "application/json",
+                "token": "null",
+            },
+            "request_params": {
+                "id": "null",
+                "name": "null",
+                "name2": "null",
+                "name3": "null",
+                "name4": "null",
+                "name5": "null",
+
+            }
+        }
+
+    return jsonify(response_data)
+
+
+@web.route('/run_api_post', methods=['POST'])
+def run_api_post():
+    data = request.json
+    print(data)
+    url = data['send_url']
+    headers = data['send_headers']
+    payload = data['send_body']
+    res = requests.post(url=url, headers=headers, data=payload)
+    # print(res.text)
+    response = {'response_code': res.status_code, 'response_text': res.text}
+    return jsonify(response)
+
+@web.route('/run_api_get', methods=['POST'])
+def run_api_get():
+    data = request.json
+    print(data)
+    url = data['send_url']
+    headers = data['send_headers']
+    payload = data['send_body']
+    res = requests.get(url=url, headers=headers, data=payload)
+    # print(res.text)
+    response = {'response_code': res.status_code, 'response_text': res.text}
+    return jsonify(response)
 
